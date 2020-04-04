125c64cb0ff8e9a80533201ef4faab0cb280d529
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-04-04 19:31:58 +0200

    Use effect for running the setup once

--- src/components/user/bookings/List.tsx
+++ src/components/user/bookings/List.tsx
@@ -1,11 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {useFetch} from 'hooks/useFetch';
 import BookingItem from './Item';
 import UserContext from 'contexts/UserContext';
 import BookingsContext from 'contexts/BookingsContext';
 import BookingItemInterface from 'interfaces/BookingItemInterface';
 const host = process.env.REACT_APP_API_HOST;
-let initializedWithContext = false;
 
 function BookingsList() {
   const {user} = useContext(UserContext);
@@ -15,13 +14,12 @@ function BookingsList() {
   const url = `${host}/user/bookings/requested`;
   const {error, loading, results} = useFetch(url, user.token);
 
+  useEffect(() => {
+    setBookings(results);
+  }, [results, setBookings]);
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error while fetching data.</div>;
-
-  if (!initializedWithContext) {
-    setBookings(results);
-    initializedWithContext = true;
-  }
 
   const showBookingsList = () => {
     setActive(active ? false : true);
@@ -30,23 +28,27 @@ function BookingsList() {
   return (
     <div className='bookings'>
       <button onClick={showBookingsList}>Booking requests</button>
-      <ul className={active ? 'active' : ''}>
-        {bookings.map((row: BookingItemInterface) => {
-          return (
-            <BookingItem
-              id={row.id}
-              requester_id={row.requester_id}
-              requester_type={row.requester_type}
-              requested_id={row.requested_id}
-              requested_type={row.requested_type}
-              booking_date={row.booking_date}
-              performer_name={row.performer_name}
-              venue_name={row.venue_name}
-              key={`booking-${row.id}`}
-            />
-          );
-        })}
-      </ul>
+      <div className={`list ${active ? 'active' : ''}`}>
+        {(bookings.length > 0 && (
+          <ul>
+            {bookings.map((row: BookingItemInterface) => {
+              return (
+                <BookingItem
+                  id={row.id}
+                  requester_id={row.requester_id}
+                  requester_type={row.requester_type}
+                  requested_id={row.requested_id}
+                  requested_type={row.requested_type}
+                  booking_date={row.booking_date}
+                  performer_name={row.performer_name}
+                  venue_name={row.venue_name}
+                  key={`booking-${row.id}`}
+                />
+              );
+            })}
+          </ul>
+        )) || <div>No booking requests yet.</div>}
+      </div>
     </div>
   );
 }
