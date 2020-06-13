be30d027e217dd7aea55787d4d28c5ec87210d1f
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-06-13 22:42:39 +0200

    Notification styles

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import NotificationContext from 'contexts/NotificationContext';
 
+import '../styles/components/Notification.scss';
+
 function Notification() {
-  const {notification} = React.useContext(NotificationContext);
+  const {notification} = useContext(NotificationContext);
+  const [active, setActive] = useState<boolean>(
+    notification.type !== '' && notification.message !== ''
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => setActive(false), 4000);
+    setActive(notification.type !== '' && notification.message !== '');
+    return () => clearTimeout(timer);
+  }, [notification]);
 
   return (
-    <div>
-      {notification.type && notification.message && (
-        <p>
-          <b>{notification.type.toUpperCase()}</b>: {notification.message}
-        </p>
+    <div className={`notification ${active ? 'active' : ''}`}>
+      {active && (
+        <div className='notification-content'>
+          <div className={`side ${notification.type}`}></div>
+          <p>
+            <b>{notification.type.toUpperCase()}</b>: {notification.message}
+          </p>
+        </div>
       )}
     </div>
   );
